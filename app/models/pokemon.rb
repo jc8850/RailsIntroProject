8187b7c2f51dce9f6590fbcee0acdd8cdf8a96b2
JC Naniong <jnaniong@academic.rrc.ca>
2022-10-12 20:56:12 -0500

    added many to many relationship in pokemon and types models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,7 @@
 class Pokemon < ApplicationRecord
   validates :name, presence true, uniqueness: true
   validates :imgLink, presence true, uniqueness: true
+
+  has_many :poke_types
+  has_many :types, through: :poke_types
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,6 @@
 class Type < ApplicationRecord
   validates :name, presence true, uniqueness: true
+
+  has_many :poke_types
+  has_many :pokemons, through: :poke_types
 end
